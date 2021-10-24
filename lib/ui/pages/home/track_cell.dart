c92eca995fadbccb11a08a3edc1265bd1fb28f9f
Daniel Hakimi <>
2021-10-24 03:22:31 +0200

    - added tap event to navigate to details page

--- lib/ui/pages/home/track_cell.dart
+++ lib/ui/pages/home/track_cell.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:lastfm_client/models/track.dart';
+import 'package:lastfm_client/ui/pages/details_view/details_page.dart';
 import 'package:lastfm_client/ui/widgets/height_spacer.dart';
+import 'package:lastfm_client/ui/widgets/width_spacer.dart';
 import 'package:lastfm_client/util/image_extension.dart';
 import 'package:lastfm_client/models/image.dart' as img;
 
@@ -15,35 +18,48 @@ class TrackCell extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const HeightSpacer(),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.network(
-                track.image.getImageUrlFrom(size: img.Size.small),
-                width: 40,
-                height: 40,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      track.name,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Text(track.artist),
-                  ],
-                ),
-              ),
-            ],
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => DetailsPage(track: track),
           ),
-          const HeightSpacer(),
-        ],
+        );
+      },
+      child: Container(
+        color: Colors.transparent,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const HeightSpacer(),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const WidthSpacer(
+                  width: 8.0,
+                ),
+                Image.network(
+                  track.image.getImageUrlFrom(size: img.Size.small),
+                  width: 40,
+                  height: 40,
+                ),
+                const WidthSpacer(width: 8.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        track.name,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(track.artist),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const HeightSpacer(),
+          ],
+        ),
       ),
     );
   }
